5ebfea04d8e72837a967e0bdb8c53612c35a735b
chanoudomchim <chanoudomchim7@gmail.com>
2026-01-03 18:58:48 +0700

    Add files via upload

---
+++ web-api-product/src/main/java/com/setec/confi/MyConfi.java
@@ -0,0 +1,51 @@
+package com.setec.confi;
+
+import java.io.File;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+
+@Configuration
+public class MyConfi implements WebMvcConfigurer{
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler("/static/**")
+			
+				//addResourceLocations("file:./myApp/static/"); //for windows
+			
+				.addResourceLocations("file:/opt/myApp/static/"); //for Linux
+		//ls -ld /opt/myApp/static
+		//chmod -R 755 /opt/myApp/static
+		
+	}
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        // Explicitly define the server URL for Swagger UI API calls
+		
+        Server productionServer = new Server();
+        productionServer.setUrl("https://cors.setecist.uk"); 
+        productionServer.setDescription("Production Server");
+
+        // You can add other servers (e.g., a local HTTP one for development)
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Development Server");
+
+        return new OpenAPI()
+                .servers(List.of(productionServer, devServer))
+                // Add other configurations like info, security schemes, etc.
+                ;
+    }
+	
+	
+}
